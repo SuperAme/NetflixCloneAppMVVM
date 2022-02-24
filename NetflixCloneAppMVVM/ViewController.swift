e4695b95755c6d4a2236fa592b12b62761e41a48
Americo Meneses <beatamez@hotmail.com>
2022-02-24 14:39:06 -0600

    Initial Commit

---
+++ NetflixCloneAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixCloneAppMVVM
+//
+//  Created by Américo MQ on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
