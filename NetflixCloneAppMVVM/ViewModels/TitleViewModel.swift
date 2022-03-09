d4562d94dfd11884ef9bcb6f55bce5aa544c2a59
Americo Meneses <beatamez@hotmail.com>
2022-03-09 12:08:20 -0600

    finishing the upcoming section

---
+++ NetflixCloneAppMVVM/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixCloneAppMVVM
+//
+//  Created by Américo MQ on 09/03/22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterUrl: String
+}
