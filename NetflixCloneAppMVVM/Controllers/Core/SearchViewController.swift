5586fdcd38e79d2d95284abc9995bbdb26c8c3d0
Americo Meneses <beatamez@hotmail.com>
2022-03-24 17:51:54 -0600

    search functionality is working

--- NetflixCloneAppMVVM/Controllers/Core/SearchViewController.swift
+++ NetflixCloneAppMVVM/Controllers/Core/SearchViewController.swift
@@ -40,6 +40,7 @@ class SearchViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .white
         
         fetchDiscoverMovies()
+        searchController.searchResultsUpdater = self
     }
     
     private func fetchDiscoverMovies() {
@@ -80,5 +81,29 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 120
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else { return }
+        
+        APICaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            
+            }
+        }
     }
 }
